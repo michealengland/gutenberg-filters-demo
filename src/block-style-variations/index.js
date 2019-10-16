d222d1334b3472daa0210b18f3b8adb0c2995827
Mike England <mike.england@webdevstudios.com>
2019-10-16 01:12:22 -0400

    Setup block style variations demo.

---
+++ src/block-style-variations/index.js
@@ -0,0 +1,25 @@
+/**
+ * Based on: Block Style Variations
+ *
+ * @url https://developer.wordpress.org/block-editor/developers/filters/block-filters/#block-style-variations
+ */
+// Add a class name to core gallery.
+wp.blocks.registerBlockStyle( 'core/gallery', {
+	name: 'roundy-round', // string for class name.
+	label: 'Roundy Round Borders', // Label displayed in the editor.
+	// isDefault: true,
+} );
+
+// Add another class name.
+wp.blocks.registerBlockStyle( 'core/gallery', {
+	name: 'blocky-block', // string for class name.
+	label: 'Blocky Block Gallery', // Label displayed in the editor.
+} );
+
+// Remove block style.
+// wp.blocks.unregisterBlockStyle( 'core/gallery', 'blocky-block' );
+
+// Remove block style once the DOM is fully loaded.
+// wp.domReady( function() {
+// 	wp.blocks.unregisterBlockStyle( 'core/gallery', 'blocky-block' );
+// } );
