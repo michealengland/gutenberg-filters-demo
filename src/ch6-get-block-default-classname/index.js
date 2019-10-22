791352c30fad2c8eca12f7beaf94976183bf74bc
Mike England <mike.england@webdevstudios.com>
2019-10-21 22:57:57 -0400

    Setup ch6 class filter

---
+++ src/ch6-get-block-default-classname/index.js
@@ -0,0 +1,19 @@
+/**
+ * Filter the class names of a block.
+ *
+ * @param {*} className
+ * @param {*} blockName
+ * @link https://developer.wordpress.org/block-editor/developers/filters/block-filters/#blocks-getblockdefaultclassname
+ */
+
+// Our filter function
+function setBlockCustomClassName( className, blockName ) {
+	return blockName === 'core/code' ? 'block-custom-class' : className;
+}
+
+// Adding the filter.
+wp.hooks.addFilter(
+	'blocks.getBlockDefaultClassName',
+	'core/code',
+	setBlockCustomClassName
+);
