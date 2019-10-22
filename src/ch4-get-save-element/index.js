1938a891931da9a914a820056ab1c57995e645a1
Mike England <mike.england@webdevstudios.com>
2019-10-21 22:13:44 -0400

    Finished chapter 4.

---
+++ src/ch4-get-save-element/index.js
@@ -0,0 +1,33 @@
+/**
+ * Adds a class to the core/list block called wp-block-list.
+ *
+ * @param element
+ * @param blockType
+ * @param attributes
+ * @return Object
+ *
+ * @link https://developer.wordpress.org/block-editor/developers/filters/block-filters/#blocks-registerblocktype
+ */
+
+function modifySavedElements( element, blockType, blockAttributes ) {
+	if ( blockType.name !== 'core/list' ) {
+		return element;
+	}
+
+	// console.log( element );
+	// console.log( blockType );
+	// console.log( blockAttributes );
+
+	return (
+
+		<div className={ 'modified-saved-element' }>
+			{ element }
+		</div>
+	);
+}
+
+wp.hooks.addFilter(
+	'blocks.getSaveElement',
+	'core/list-block',
+	modifySavedElements
+);
