18e5c90109709276f3e35440a6ffee48c208afb1
Mike England <mike.england@webdevstudios.com>
2019-10-21 21:17:21 -0400

    Finalized chapter 3.

--- src/ch3-register-block-type/index.js
+++ src/ch3-register-block-type/index.js
@@ -7,17 +7,26 @@
  *
  * @link https://developer.wordpress.org/block-editor/developers/filters/block-filters/#blocks-registerblocktype
  */
+
+const { __ } = window.wp.i18n;
+
 function addListBlockClassName( settings, name ) {
 	// returns if block is not in the editor.
 	if ( name !== 'core/list' ) {
 		return settings;
 	}
 
+	// How to view block properties.
+	// console.log( settings );
+
 	// Enableds the a default class name for this block.
 	// eslint-disable-next-line no-undef
 	return lodash.assign(
 		{},
 		settings, {
+			keywords: [
+				__( 'Scruffy Nerf Herder' ),
+			],
 			// eslint-disable-next-line no-undef
 			supports: lodash.assign( {}, settings.supports, {
 				className: true,
