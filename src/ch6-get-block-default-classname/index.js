393e2b7b1d795fd8512613a0aef1a9894efc3c0a
Mike England <mike.england@webdevstudios.com>
2019-10-21 22:59:54 -0400

    Added logs for parameters.

--- src/ch6-get-block-default-classname/index.js
+++ src/ch6-get-block-default-classname/index.js
@@ -8,6 +8,9 @@
 
 // Our filter function
 function setBlockCustomClassName( className, blockName ) {
+	// console.log( className );
+	// console.log( blockName );
+
 	return blockName === 'core/code' ? 'block-custom-class' : className;
 }
 
